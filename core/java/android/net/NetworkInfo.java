df6ff6c54c6c1d9db655a7912f6aa1b0edbc4cae
John Joseph <jnjoseph@codeaurora.org>
2010-12-20 15:08:26 -0800

    net: add apis in networkinfo to report if v4,v6 is connected
    - add api mIsIpV4Connected() and mIsIpV6Connected() to report v4,v6
      states in networkInfo.
    
    CRs-Fixed : 251691
    
    Change-Id: I9faa096886ae15462ff16c4ba874f5d410a71e9f

--- core/java/android/net/NetworkInfo.java
+++ core/java/android/net/NetworkInfo.java
@@ -108,6 +108,9 @@ public class NetworkInfo implements Parcelable {
     private String mExtraInfo;
     private boolean mIsFailover;
     private boolean mIsRoaming;
+    private boolean mIsIpV4Connected;
+    private boolean mIsIpV6Connected;
+
     /**
      * Indicates whether network connectivity is possible:
      */
@@ -131,7 +134,7 @@ public class NetworkInfo implements Parcelable {
         mSubtype = subtype;
         mTypeName = typeName;
         mSubtypeName = subtypeName;
-        setDetailedState(DetailedState.IDLE, null, null);
+        setDetailedState(DetailedState.IDLE, false, false, null, null);
         mState = State.UNKNOWN;
         mIsAvailable = false; // until we're told otherwise, assume unavailable
         mIsRoaming = false;
@@ -197,6 +200,34 @@ public class NetworkInfo implements Parcelable {
      */
     public boolean isConnected() {
         return mState == State.CONNECTED;
+    }
+
+    /**
+     * Indicates whether ipv4 network connectivity exists and it is possible to establish
+     * connections and pass ipv4 data.
+     * @return {@code true} if network connectivity through ipv4 exists, {@code false} otherwise.
+     *
+     * @hide
+     */
+    public boolean isIpv4Connected() {
+        if (isConnected()) {
+            return mIsIpV4Connected;
+        }
+        return false;
+    }
+
+    /**
+     * Indicates whether ipv6 network connectivity exists and it is possible to establish
+     * connections and pass ipv6 data.
+     * @return {@code true} if network connectivity through ipv6 exists, {@code false} otherwise.
+     *
+     * @hide
+     */
+    public boolean isIpv6Connected() {
+        if (isConnected()) {
+            return mIsIpV6Connected;
+        }
+        return false;
     }
 
     /**
@@ -284,11 +315,19 @@ public class NetworkInfo implements Parcelable {
      * @param extraInfo an optional {@code String} providing addditional network state
      * information passed up from the lower networking layers.
      */
-    void setDetailedState(DetailedState detailedState, String reason, String extraInfo) {
+    void setDetailedState(DetailedState detailedState, boolean isIpv4Connected,
+            boolean isIpv6Connected, String reason, String extraInfo) {
         this.mDetailedState = detailedState;
         this.mState = stateMap.get(detailedState);
         this.mReason = reason;
         this.mExtraInfo = extraInfo;
+        if (this.mState != State.CONNECTED) {
+            this.mIsIpV4Connected = false;
+            this.mIsIpV6Connected = false;
+        } else {
+            this.mIsIpV4Connected = isIpv4Connected;
+            this.mIsIpV6Connected = isIpv6Connected;
+        }
     }
 
     /**
@@ -333,7 +372,9 @@ public class NetworkInfo implements Parcelable {
                 append(", extra: ").append(mExtraInfo == null ? "(none)" : mExtraInfo).
                 append(", roaming: ").append(mIsRoaming).
                 append(", failover: ").append(mIsFailover).
-                append(", isAvailable: ").append(mIsAvailable);
+                append(", isAvailable: ").append(mIsAvailable).
+                append(", isIpv4Connected: ").append(mIsIpV4Connected).
+                append(", isIpv6Connected: ").append(mIsIpV6Connected);
         return builder.toString();
     }
 
@@ -361,7 +402,8 @@ public class NetworkInfo implements Parcelable {
         dest.writeInt(mIsRoaming ? 1 : 0);
         dest.writeString(mReason);
         dest.writeString(mExtraInfo);
-        dest.writeString(mInterfaceName);
+        dest.writeInt(mIsIpV4Connected? 1 : 0);
+        dest.writeInt(mIsIpV6Connected? 1 : 0);
     }
 
     /**
@@ -383,7 +425,8 @@ public class NetworkInfo implements Parcelable {
                 netInfo.mIsRoaming = in.readInt() != 0;
                 netInfo.mReason = in.readString();
                 netInfo.mExtraInfo = in.readString();
-                netInfo.mInterfaceName = in.readString();
+                netInfo.mIsIpV4Connected = in.readInt() != 0;
+                netInfo.mIsIpV6Connected = in.readInt() != 0;
                 return netInfo;
             }
 
