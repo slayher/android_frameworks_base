Jackeagle/caf_frameworks_base

Data call teardown: Include reason parameter
Change-Id: I0d501ba1d15371f4c98ce752f0230b19023929c0
CRs-Fixed: 275975

telephony/java/com/android/internal/telephony/MMDataConnectionTracker.java
@@ -1333,12 +1333,7 @@ public class MMDataConnectionTracker extends DataConnectionTracker {
 
         mDesiredPowerState = desiredPowerState;
 
-        /*
-         * TODO: fix this workaround. For 1x, we should not disconnect data call
-         * before powering off.
-         */
-
-        if (mDesiredPowerState == false && getRadioTechnology() != RadioTechnology.RADIO_TECH_1xRTT) {
+        if (mDesiredPowerState == false) {
             disconnectAllConnections(Phone.REASON_RADIO_TURNED_OFF, onCompleteMsg);
             return;
         }
@@ -1477,7 +1472,14 @@ public class MMDataConnectionTracker extends DataConnectionTracker {
         c.dc = dc;
         c.reason = reason;
 
-        dc.disconnect(obtainMessage(EVENT_DISCONNECT_DONE, c));
+        int dcReason = 0;
+        // Set the reason. Currently indicating explicit powerdown reasons only
+        if (Phone.REASON_RADIO_TURNED_OFF.equals(reason))
+            dcReason = DEACTIVATE_REASON_RADIO_OFF;
+        else
+            dcReason = DEACTIVATE_REASON_NONE;
+
+        dc.disconnect(obtainMessage(EVENT_DISCONNECT_DONE, dcReason, 0, c));
         return true;
     }
 

slayher/android_frameworks_base

Change-Id: I063352429b43e277adb16b49921f5ed879a03a55

telephony/java/com/android/internal/telephony/MMDataConnectionTracker.java
@@ -1261,12 +1261,7 @@ public class MMDataConnectionTracker extends DataConnectionTracker {
         mDesiredPowerState = desiredPowerState;
         mPendingPowerOffCompleteMsg = null;
 
-        /*
-         * TODO: fix this workaround. For 1x, we should not disconnect data call
-         * before powering off.
-         */
-
-        if (mDesiredPowerState == false && getRadioTechnology() != RadioTechnology.RADIO_TECH_1xRTT) {
+        if (mDesiredPowerState == false) {
             mPendingPowerOffCompleteMsg = onCompleteMsg;
             disconnectAllConnections(Phone.REASON_RADIO_TURNED_OFF);
             return;
@@ -1391,18 +1386,26 @@ public class MMDataConnectionTracker extends DataConnectionTracker {
         }
     }
 
-    private boolean tryDisconnectDataCall(DataConnection dc, String reason) {
-        logv("tryDisconnectDataCall : dc=" + dc + ", reason=" + reason);
-        dc.disconnect(obtainMessage(EVENT_DISCONNECT_DONE, reason));
-        return true;
-    }
-
     class CallbackData {
         DataConnection dc;
         DataProfile dp;
         IPVersion ipv;
         String reason;
         DataServiceType ds;
+    }
+
+    private boolean tryDisconnectDataCall(DataConnection dc, String reason) {
+        logv("tryDisconnectDataCall : dc=" + dc + ", reason=" + reason);
+
+        int dcReason = 0;
+        // Set the reason. Currently indicating explicit powerdown reasons only
+        if (Phone.REASON_RADIO_TURNED_OFF.equals(reason))
+            dcReason = DEACTIVATE_REASON_RADIO_OFF;
+        else
+            dcReason = DEACTIVATE_REASON_NONE;
+
+        dc.disconnect(obtainMessage(EVENT_DISCONNECT_DONE, dcReason, 0, reason));
+        return true;
     }
 
     private boolean trySetupDataCall(DataServiceType ds, IPVersion ipv, String reason) {